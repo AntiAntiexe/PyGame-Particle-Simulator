32b25f1f4292880b6030b4349c7c28eb313cc910
AntiAntiexe <mrbricken@gmail.com>
2024-09-21 20:59:31 +1000

    Improve Movement

--- fluid-sim/old_scripts/fluid_no_class.py
+++ fluid-sim/old_scripts/fluid_no_class.py
@@ -3,15 +3,14 @@ import numpy as np
 import math
 
 
-
 space_size = 500
 
-rate = 60 # fps
-dt = 1/rate # times step between frames
+rate = 60  # fps
+dt = 1 / rate  # times step between frames
 
 # Particle One
 pos_1 = np.array([125, 125], dtype=np.float64)
-#collisionDamp = 1
+# collisionDamp = 1
 velocity1 = 300
 gravity1 = 50
 r1 = 10
@@ -40,7 +39,6 @@ clock = pygame.time.Clock()
 # Run until the user asks to quit
 running = True
 while running:
-
     # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -50,39 +48,44 @@ while running:
     screen.fill((0, 0, 0))
 
     # Draw a solid white circle in the center
-    pygame.draw.circle(screen, colour1, pos_1,r1)
+    pygame.draw.circle(screen, colour1, pos_1, r1)
 
     # Draw a solid white circle in the center
     pygame.draw.circle(screen, colour2, pos_2, r2)
 
-    if abs(math.sqrt((pos_2[0] - pos_1[0])**2 + (pos_2[1] - pos_1[1])**2)) < r1+r2:
+    if (
+        abs(math.sqrt((pos_2[0] - pos_1[0]) ** 2 + (pos_2[1] - pos_1[1]) ** 2))
+        < r1 + r2
+    ):
         v1, v2 = velocity1, velocity2
 
-        velocity1 = v1 - (2 * m2)/(m1 + m2) * (np.dot((v1 - v2), (pos_1[0] - pos_2[0])))/((math.sqrt(abs(pos_1[0]- pos_2[0])**2))**2) * (pos_1[0] - pos_2[0])
-        velocity2 = v2 - (2 * m1) / (m1 + m2) * (np.dot((v2 - v1), (pos_2[0] - pos_1[0]))) / ((math.sqrt(abs(pos_2[0] - pos_1[0]) ** 2)) ** 2) * (pos_2[0] - pos_1[0])
+        velocity1 = v1 - (2 * m2) / (m1 + m2) * (
+            np.dot((v1 - v2), (pos_1[0] - pos_2[0]))
+        ) / ((math.sqrt(abs(pos_1[0] - pos_2[0]) ** 2)) ** 2) * (pos_1[0] - pos_2[0])
+        velocity2 = v2 - (2 * m1) / (m1 + m2) * (
+            np.dot((v2 - v1), (pos_2[0] - pos_1[0]))
+        ) / ((math.sqrt(abs(pos_2[0] - pos_1[0]) ** 2)) ** 2) * (pos_2[0] - pos_1[0])
     # Wall collisions particle one
-    if pos_1[0]-r1 <= 0 or pos_1[0]+r1 >= space_size:
+    if pos_1[0] - r1 <= 0 or pos_1[0] + r1 >= space_size:
         velocity1 = -velocity1
 
-    if pos_1[1]-r1 <= 0 or pos_1[1]+r1 >= space_size:
+    if pos_1[1] - r1 <= 0 or pos_1[1] + r1 >= space_size:
         gravity1 = -gravity1
-        #y1 = (y1-r1) * y1
-        #gravity *= -1 * collisionDamp
+        # y1 = (y1-r1) * y1
+        # gravity *= -1 * collisionDamp
 
-    if pos_2[0]-r2 <= 0 or pos_2[0]+r2 >= space_size:
+    if pos_2[0] - r2 <= 0 or pos_2[0] + r2 >= space_size:
         velocity2 = -velocity2
 
-    if pos_2[1]-r2 <= 0 or pos_2[1]+r2 >= space_size:
+    if pos_2[1] - r2 <= 0 or pos_2[1] + r2 >= space_size:
         gravity2 = -gravity2
-        #y1 = (y1-r1) * y1
-        #gravity *= -1 * collisionDamp
+        # y1 = (y1-r1) * y1
+        # gravity *= -1 * collisionDamp
 
     # Flip the display
     pygame.display.flip()
 
     pos_1[1] = pos_1[1] + gravity1 * dt
-
-
 
     pos_1[0] = pos_1[0] + velocity1 * dt
 
@@ -92,11 +95,10 @@ while running:
 
     print(pos_1[0], pos_1[1], pos_2[0], pos_2[1])
 
-
-
     # limit frame rate to desired number of frames per second.
 
     clock.tick(rate)
 
 # Done! Time to quit.
 pygame.quit()
+
